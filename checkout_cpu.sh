0de819d18fe655deb3107cdf7c92d280fa0e3dff
Xinsheng (Shawn) Qin <xinshengqin2014@gmail.com>
2018-04-27 17:58:59 -0700

    update checkout_cpu.sh script

--- checkout_cpu.sh
+++ checkout_cpu.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cd ./amrclaw  && git checkout v5.4.1 && cd ../ && \
+cd ./amrclaw  && git checkout local_based_on_v5.4.1 && cd ../ && \
+cd ./clawutil && git checkout local_based_on_v5.4.1 && cd ../ && \
+cd ./riemann  && git checkout local_based_on_v5.4.1 && cd ../ && \
 cd ./classic  && git checkout v5.4.1 && cd ../ && \
-cd ./clawutil && git checkout v5.4.1 && cd ../ && \
 cd ./pyclaw   && git checkout v5.4.1 && cd ../ && \
-cd ./riemann  && git checkout v5.4.1 && cd ../ && \
 cd ./visclaw  && git checkout v5.4.1 && cd ../ && \
-cd ./clawpack && git checkout v5.4.1 && cd ../ && \
+# cd ./clawpack && git checkout v5.4.1 && cd ../ && \
 cd ./geoclaw  && git checkout v5.4.1 && cd ../
