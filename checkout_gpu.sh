a405b14eb06ceb5cff8b67e94e6be19a7bc33f2e
Xinsheng (Shawn) Qin <xinshengqin2014@gmail.com>
2018-07-14 11:37:57 -0700

    update checkout_gpu.sh

--- checkout_gpu.sh
+++ checkout_gpu.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd ./amrclaw  && git checkout no_unified_memory && cd ../ && \
+# cd ./amrclaw  && git checkout no_unified_memory && cd ../ && \
+cd ./amrclaw  && git checkout    merge_cudaclaw && cd ../ && \
 cd ./clawutil && git checkout           gpu_amr && cd ../ && \
-cd ./riemann  && git checkout            gpu_amr && cd ../
+cd ./riemann  && git checkout           gpu_amr && cd ../
