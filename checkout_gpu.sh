84e597a4d4130f97ebc534bb02acf9c4c69f1cdb
Xinsheng (Shawn) Qin <xinshengqin2014@gmail.com>
2018-07-31 18:27:18 -0700

    update checkout_gpu.sh after adding GPU support to geoclaw

--- checkout_gpu.sh
+++ checkout_gpu.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# cd ./amrclaw  && git checkout no_unified_memory && cd ../ && \
+
 cd ./amrclaw  && git checkout    merge_cudaclaw && cd ../ && \
 cd ./clawutil && git checkout           gpu_amr && cd ../ && \
-cd ./riemann  && git checkout           gpu_amr && cd ../
+cd ./riemann  && git checkout           gpu_amr && cd ../ && \
+cd ./geoclaw  && git checkout           gpu && cd ../
+cd ./classic  && git checkout v5.4.1 && cd ../ && \
+cd ./pyclaw   && git checkout v5.4.1 && cd ../ && \
+cd ./visclaw  && git checkout v5.4.1 && cd ../
