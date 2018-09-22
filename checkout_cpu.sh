be5cd4ecc8f5b0ed9afe3ff256e1c652a462cab2
Xinsheng (Shawn) Qin <xinshengqin2014@gmail.com>
2018-09-22 13:34:38 -0700

    update checkout scripts

--- checkout_cpu.sh
+++ checkout_cpu.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-cd ./amrclaw  && git checkout local_based_on_v5.4.1 && cd ../ && \
-cd ./clawutil && git checkout local_based_on_v5.4.1 && cd ../ && \
-cd ./riemann  && git checkout local_based_on_v5.4.1 && cd ../ && \
-cd ./classic  && git checkout v5.4.1 && cd ../ && \
-cd ./pyclaw   && git checkout v5.4.1 && cd ../ && \
-cd ./visclaw  && git checkout v5.4.1 && cd ../ && \
-# cd ./clawpack && git checkout v5.4.1 && cd ../ && \
-cd ./geoclaw  && git checkout v5.4.1 && cd ../
+
+# cd ./riemann  && git checkout local_based_on_v5.4.1 && cd ../ && \
+# cd ./geoclaw  && git checkout local_based_on_v5.4.1 && cd ../ && \
+# cd ./clawutil && git checkout local_based_on_v5.4.1 && cd ../ && \
+# cd ./amrclaw  && git checkout local_based_on_v5.4.1 && cd ../ && \
+# cd ./classic  && git checkout v5.4.1 && cd ../ && \
+# cd ./pyclaw   && git checkout v5.4.1 && cd ../ && \
+# cd ./visclaw  && git checkout v5.4.1 && cd ../ 
+
+VERSION=v5.4.1
+cd ./riemann  && git checkout $VERSION && cd ../ && \
+cd ./geoclaw  && git checkout $VERSION && cd ../ && \
+cd ./clawutil && git checkout $VERSION && cd ../ && \
+cd ./amrclaw  && git checkout $VERSION && cd ../ && \
+cd ./classic  && git checkout $VERSION && cd ../ && \
+cd ./pyclaw   && git checkout $VERSION && cd ../ && \
+cd ./visclaw  && git checkout $VERSION && cd ../ 
--- checkout_gpu.sh
+++ checkout_gpu.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 cd ./amrclaw  && git checkout    merge_cudaclaw && cd ../ && \
+# cd ./amrclaw  && git checkout    debug_gpu && cd ../ && \
 cd ./clawutil && git checkout           gpu_amr && cd ../ && \
 cd ./riemann  && git checkout           gpu_amr && cd ../ && \
+# cd ./riemann  && git checkout           debug_gpu && cd ../ && \
 cd ./geoclaw  && git checkout           gpu && cd ../
+# cd ./geoclaw  && git checkout           debug_gpu && cd ../
 cd ./classic  && git checkout v5.4.1 && cd ../ && \
 cd ./pyclaw   && git checkout v5.4.1 && cd ../ && \
 cd ./visclaw  && git checkout v5.4.1 && cd ../
