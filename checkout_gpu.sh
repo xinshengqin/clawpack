1558fc5d9d4c6b5bb605a5632949df5d0f1db3fc
Xinsheng (Shawn) Qin <xinshengqin2014@gmail.com>
2018-11-08 18:18:26 -0800

    update checkout scripts

--- checkout_gpu.sh
+++ checkout_gpu.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-cd ./amrclaw  && git checkout    merge_cudaclaw && cd ../ && \
-# cd ./amrclaw  && git checkout    debug_gpu && cd ../ && \
-cd ./clawutil && git checkout           gpu_amr && cd ../ && \
-cd ./riemann  && git checkout           gpu_amr && cd ../ && \
-# cd ./riemann  && git checkout           debug_gpu && cd ../ && \
-cd ./geoclaw  && git checkout           gpu && cd ../
-# cd ./geoclaw  && git checkout           debug_gpu && cd ../
-cd ./classic  && git checkout v5.4.1 && cd ../ && \
-cd ./pyclaw   && git checkout v5.4.1 && cd ../ && \
-cd ./visclaw  && git checkout v5.4.1 && cd ../
+echo "checkout amrclaw ===================================>" && \
+    # cd ./amrclaw  && git checkout gpu_amr && cd ../ && \
+    cd ./amrclaw  && git checkout merge_adjoint && cd ../ && \
+echo "checkout clawutil ===================================>" && \
+    cd ./clawutil && git checkout gpu_amr && cd ../ && \
+echo "checkout riemann ===================================>" && \
+    # cd ./riemann  && git checkout gpu_amr && cd ../ && \
+    cd ./riemann  && git checkout merge_adjoint && cd ../ && \
+echo "checkout geoclaw ===================================>" && \
+    # cd ./geoclaw  && git checkout gpu && cd ../
+    cd ./geoclaw  && git checkout merge_adjoint && cd ../
+echo "checkout classic ===================================>" && \
+    cd ./classic  && git checkout shawn_dev && cd ../ && \
+echo "checkout pyclaw ===================================>" && \
+    cd ./pyclaw   && git checkout shawn_dev && cd ../ && \
+echo "checkout visclaw ===================================>" && \
+    cd ./visclaw  && git checkout shawn_dev && cd ../
+
+# add fp32
+# cd ./amrclaw  && git checkout           add_fp32 && cd ../ && \
+# cd ./clawutil && git checkout           gpu_amr && cd ../ && \
+# cd ./riemann  && git checkout           add_fp32 && cd ../ && \
+# cd ./geoclaw  && git checkout           add_fp32 && cd ../
+# cd ./classic  && git checkout v5.4.1 && cd ../ && \
+# cd ./pyclaw   && git checkout v5.4.1 && cd ../ && \
+# cd ./visclaw  && git checkout v5.4.1 && cd ../
--- show_version.sh
+++ show_version.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
+echo "clawpack:" && \
+cd ./clawpack && (git symbolic-ref -q --short HEAD || git describe --tags --exact-match) && cd ../ && \
+echo " " && \
+
 echo "amrclaw:" && \
 cd ./amrclaw  && (git symbolic-ref -q --short HEAD || git describe --tags --exact-match) && cd ../ && \
 echo " " && \
 
-echo "classic:" && \
-cd ./classic  && (git symbolic-ref -q --short HEAD || git describe --tags --exact-match) && cd ../ && \
+echo "geoclaw:" && \
+cd ./geoclaw  && (git symbolic-ref -q --short HEAD || git describe --tags --exact-match) && cd ../
 echo " " && \
 
 echo "clawutil:" && \
 cd ./clawutil && (git symbolic-ref -q --short HEAD || git describe --tags --exact-match) && cd ../ && \
-echo " " && \
-
-echo "pyclaw:" && \
-cd ./pyclaw   && (git symbolic-ref -q --short HEAD || git describe --tags --exact-match) && cd ../ && \
 echo " " && \
 
 echo "riemann:" && \
@@ -23,10 +23,10 @@ echo "visclaw:" && \
 cd ./visclaw  && (git symbolic-ref -q --short HEAD || git describe --tags --exact-match) && cd ../ && \
 echo " " && \
 
-echo "clawpack:" && \
-cd ./clawpack && (git symbolic-ref -q --short HEAD || git describe --tags --exact-match) && cd ../ && \
+echo "pyclaw:" && \
+cd ./pyclaw   && (git symbolic-ref -q --short HEAD || git describe --tags --exact-match) && cd ../ && \
 echo " " && \
 
-echo "geoclaw:" && \
-cd ./geoclaw  && (git symbolic-ref -q --short HEAD || git describe --tags --exact-match) && cd ../
+echo "classic:" && \
+cd ./classic  && (git symbolic-ref -q --short HEAD || git describe --tags --exact-match) && cd ../ 
 
