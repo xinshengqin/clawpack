24591900573c4f890a286d7082352e47368e6ffe
Xinsheng (Shawn) Qin <xinshengqin2014@gmail.com>
2018-11-13 23:09:33 -0800

    update checkout scripts

--- checkout_cpu.sh
+++ checkout_cpu.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-# cd ./riemann  && git checkout local_based_on_v5.4.1 && cd ../ && \
-# cd ./geoclaw  && git checkout local_based_on_v5.4.1 && cd ../ && \
-# cd ./clawutil && git checkout local_based_on_v5.4.1 && cd ../ && \
-# cd ./amrclaw  && git checkout local_based_on_v5.4.1 && cd ../ && \
-# cd ./classic  && git checkout v5.4.1 && cd ../ && \
-# cd ./pyclaw   && git checkout v5.4.1 && cd ../ && \
-# cd ./visclaw  && git checkout v5.4.1 && cd ../ 
+echo "checkout amrclaw ===================================>" && \
+    cd ./amrclaw  && git fetch xinshengqin && git checkout shawn_dev && cd ../ && \
+echo "checkout clawutil ===================================>" && \
+    cd ./clawutil && git fetch xinshengqin && git checkout shawn_dev && cd ../ && \
+echo "checkout riemann ===================================>" && \
+    cd ./riemann  && git fetch xinshengqin && git checkout shawn_dev && cd ../ && \
+echo "checkout geoclaw ===================================>" && \
+    cd ./geoclaw  && git fetch xinshengqin && git checkout shawn_dev && cd ../ && \
+echo "checkout visclaw ===================================>" && \
+    cd ./visclaw  && git fetch xinshengqin && git checkout shawn_dev && cd ../ && \
+echo "checkout classic ===================================>" && \
+    cd ./classic  && git fetch xinshengqin && git checkout shawn_dev && cd ../ && \
+echo "checkout pyclaw ===================================>" && \
+    cd ./pyclaw   && git fetch xinshengqin && git checkout shawn_dev && cd ../ 
 
-VERSION=v5.4.1
-cd ./riemann  && git checkout $VERSION && cd ../ && \
-cd ./geoclaw  && git checkout $VERSION && cd ../ && \
-cd ./clawutil && git checkout $VERSION && cd ../ && \
-cd ./amrclaw  && git checkout $VERSION && cd ../ && \
-cd ./classic  && git checkout $VERSION && cd ../ && \
-cd ./pyclaw   && git checkout $VERSION && cd ../ && \
-cd ./visclaw  && git checkout $VERSION && cd ../ 
+# VERSION=v5.5.0
+# cd ./riemann  && git checkout $VERSION && cd ../ && \
+# cd ./geoclaw  && git checkout $VERSION && cd ../ && \
+# cd ./clawutil && git checkout $VERSION && cd ../ && \
+# cd ./amrclaw  && git checkout $VERSION && cd ../ && \
+# cd ./classic  && git checkout $VERSION && cd ../ && \
+# cd ./pyclaw   && git checkout $VERSION && cd ../ && \
+# cd ./visclaw  && git checkout $VERSION && cd ../ 
--- checkout_gpu.sh
+++ checkout_gpu.sh
@@ -18,6 +18,23 @@ echo "checkout pyclaw ===================================>" && \
 echo "checkout visclaw ===================================>" && \
     cd ./visclaw  && git checkout shawn_dev && cd ../
 
+# # before merge adjoint and v5.5.0
+# echo "checkout amrclaw ===================================>" && \
+#     cd ./amrclaw  && git checkout gpu_amr && cd ../ && \
+# echo "checkout clawutil ===================================>" && \
+#     cd ./clawutil && git checkout gpu_amr && cd ../ && \
+# echo "checkout riemann ===================================>" && \
+#     cd ./riemann  && git checkout gpu_amr && cd ../ && \
+# echo "checkout geoclaw ===================================>" && \
+#     cd ./geoclaw  && git checkout gpu && cd ../
+# echo "checkout classic ===================================>" && \
+#     cd ./classic  && git checkout v5.4.1 && cd ../ && \
+# echo "checkout pyclaw ===================================>" && \
+#     cd ./pyclaw   && git checkout v5.4.1 && cd ../ && \
+# echo "checkout visclaw ===================================>" && \
+#     cd ./visclaw  && git checkout local_based_on_v5.4.1 && cd ../
+
+
 # add fp32
 # cd ./amrclaw  && git checkout           add_fp32 && cd ../ && \
 # cd ./clawutil && git checkout           gpu_amr && cd ../ && \
